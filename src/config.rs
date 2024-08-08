463e62b6eca2c2858848b6434e44e09d0ab0d9c9
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-08-08 07:23:13 -0700

    docs: added documentation to config

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,31 @@
 use adjustp::{adjust, Procedure};
 
+/// Configurations for weighting p-values in the GeoPAGG algorithm
 #[derive(Debug, Clone, Copy)]
 pub enum WeightConfig {
+    /// Equal weights for all p-values
     Balanced,
+    /// Weights increase linearly with rank
+    ///
+    /// n: number of p-values
+    /// i: rank of the p-value (1-indexed)
+    ///
+    /// w_i = i / n
     RankOrder,
+    /// All weights are 1.0 except the first, which is set to alpha
     DropFirst { alpha: f64 },
 }
 
 impl WeightConfig {
+    /// Builds a vector of weights based on the chosen configuration
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - The number of weights to generate
+    ///
+    /// # Returns
+    ///
+    /// A vector of f64 weights
     pub fn build_weights(&self, n: usize) -> Vec<f64> {
         match self {
             WeightConfig::Balanced => vec![1.0; n],
@@ -21,13 +39,27 @@ impl WeightConfig {
     }
 }
 
+/// Configurations for transforming p-values in the GeoPAGG algorithm
 #[derive(Debug, Clone, Copy)]
 pub enum TransformConfig {
+    /// No transformation
     Identity,
+    /// False Discovery Rate correction (Benjamini-Hochberg)
     Fdr,
+    /// Bonferroni correction
     Bonferroni,
 }
+
 impl TransformConfig {
+    /// Transforms a slice of p-values based on the chosen configuration
+    ///
+    /// # Arguments
+    ///
+    /// * `pvalues` - A slice of f64 p-values to transform
+    ///
+    /// # Returns
+    ///
+    /// A vector of transformed f64 p-values
     pub fn transform(&self, pvalues: &[f64]) -> Vec<f64> {
         match self {
             TransformConfig::Identity => pvalues.to_vec(),
